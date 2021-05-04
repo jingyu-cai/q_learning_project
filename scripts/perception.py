37027c46f806ba65d015c98c2b9f5b1625e4b2c2
ZhouXing19 <zhouxing@uchicago.edu>
2021-05-03 23:59:02 -0400

    [zhou] Added comments

--- scripts/perception.py
+++ scripts/perception.py
@@ -31,14 +31,14 @@ class DigitRecognizer(object):
 
         self.bridge = cv_bridge.CvBridge()
 
-
         self.image = []
 
         self.__scan_data = []
 
         self.__goal_dist_in_front_of_dumbell = 0.35
 
-        self.__prop = 0.15
+        # For Sensory-Motor Control in controling the speed
+        self.__prop = 0.15 
 
         self.initalized = True
     
@@ -63,6 +63,9 @@ class DigitRecognizer(object):
         
 
     def pub_vel(self, ang_v=0.0, lin_v=0.0):
+        '''
+        To publish a twist to the cmd_vel channel
+        '''
         new_twist = Twist()
         new_twist.linear.x = lin_v
         new_twist.angular.z = ang_v
@@ -90,33 +93,41 @@ class DigitRecognizer(object):
 
         M = cv2.moments(mask)
 
-        # if there are any yellow pixels found
+        # if there are any pixels found for the desired color
         if M['m00'] > 0:
             # determine the center of the yellow pixels in the image
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            err = w/2 - cx
 
-            cv2.circle(self.image, (cx, cy), 20, (255,155,0), -1)
+            # Dumbbell's distance to the center of the camera
+            err = w/2 - cx
 
             print(f"abs(err) / w : {abs(err) / w}")
 
+            # If the color center is at the front
             if abs(err) / w < 0.05:
-                # The color is at the front
+                
                 min_dist = min(self.__scan_data[-10:] + self.__scan_data[:10])
+
                 if min_dist <= self.__goal_dist_in_front_of_dumbell:
+                    # Stop the robot
                     self.pub_vel(0,0)
                     print(f"---reached dumbell of color {color}----")
                 else:
+                    # Rush STRAIGHT toward the dumbbell
                     ang_v, lin_v = self.set_vel(0, min_dist)
                     self.pub_vel(ang_v, lin_v)
+
+            # If the color center is not right at the front yet
             else:            
-                # visualize a red circle in our debugging window to indicate
-                # the center point of the yellow pixels
                 k_p = 1.0 / 1000.0
+                # Slowly turn the head, so that the color center 
+                # would be at the center of the camera
                 self.pub_vel(k_p*err, 0)
                 print(f"---turning to dumbell of color {color}----")
+        # If we cannot see any pixel of the desired color
         else:
+            # Simply turn the head clockwise, without any linear speed
             ang_v, lin_v = self.set_vel()
             self.pub_vel(ang_v, lin_v)
 
@@ -126,9 +137,7 @@ class DigitRecognizer(object):
     def image_callback(self, data):
         if (not self.initalized):
             return
-
-
-            # take the ROS message with the image and turn it into a format cv2 can use
+        # take the ROS message with the image and turn it into a format cv2 can use
         self.image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
         self.move_to_dumbell('red')
             
